9fc4c74f80b1f3fbee85d3195cbe1078c9c665be
Tristan Colgate <tristan@qubit.com>
2016-09-19 16:12:07 +0100

    Updates:
    - Customize
    - Move gc-stats into prom-client
    - rename metrics from node_ to nodejs_ (to clearly separate from
      node_exporters metrics)
    - Move eventloop lag to use seconds, rename to include eventloop
    - Use a counter for process_cpu_seconds_total
    - Move gc-stats to use seconds

--- test/defaultMetrics.js
+++ test/defaultMetrics.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var optional = require('optional');
+
 describe('defaultMetrics', function() {
 	var expect = require('chai').expect;
 	var register = require('../index').register;
@@ -53,13 +55,25 @@ describe('defaultMetrics', function() {
 	it('should add metrics to the registry', function() {
 		expect(register.getMetricsAsJSON()).to.have.length(0);
 		interval = defaultMetrics();
-		expect(register.getMetricsAsJSON()).to.have.length(9);
+
+		var gc = optional('gc-stats');
+		if(typeof gc === 'function') {
+			expect(register.getMetricsAsJSON()).to.have.length(11);
+		} else {
+			expect(register.getMetricsAsJSON()).to.have.length(9);
+		};
 	});
 
 	it('should allow blacklisting unwanted metrics', function() {
 		expect(register.getMetricsAsJSON()).to.have.length(0);
 		interval = defaultMetrics(['osMemoryHeap']);
-		expect(register.getMetricsAsJSON()).to.have.length(8);
+
+		var gc = optional('gc-stats');
+		if(typeof gc === 'function') {
+			expect(register.getMetricsAsJSON()).to.have.length(10);
+		} else {
+			expect(register.getMetricsAsJSON()).to.have.length(8);
+		};
 	});
 
 	it('should allow blacklisting all metrics', function() {
